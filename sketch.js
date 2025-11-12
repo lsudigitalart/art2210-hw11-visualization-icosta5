d7d68a1faaabcc26a0b1eae2189a030160b15c3a
icosta5 <icosta5@lsu.edu>
2025-11-12 14:10:13 -0600

    setting up

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
+let stats;
+
+function preload() {
+    stats = loadTable("Crime.csv", 'csv', 'header');
+}
+
 function setup() {
   createCanvas(200, 200);
+
+  print(stats.getRowCount());
+  for(let i = 0; i < stats.getRowCount(); i++){
+    let row = stats.getRow(i);
+    print(row);
+  }
 }
 
 function draw() {
